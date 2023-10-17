03112ed2f7bd6c53b2c3394bf8e4eecb9c73ebde
Vladimir Arger <8745@rambler.ru>
2023-10-18 00:07:54 +0400

    Add tests

--- src/test/java/ru/netology/web/WebFormTest.java
+++ src/test/java/ru/netology/web/WebFormTest.java
@@ -8,14 +8,78 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class WebFormTest {
+    /*
+    В поле фамилии и имени разрешены только русские буквы, дефисы и пробелы.
+    В поле телефона — только 11 цифр, символ + на первом месте.
+    Флажок согласия должен быть выставлен.
+     */
     @Test
-    void shouldTest() {
+    void shouldTest1() {
         open("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=callback-form]");
-        form.$("[data-test-id=name] input").setValue("Василий");
-        form.$("[data-test-id=phone] input").setValue("+79270000000");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[data-test-id=submit]").click();
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+        $("[data-test-id=name] input").setValue("Василий");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldTest2() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Иванов Василий");
+        $("[data-test-id=phone] input").setValue("+12345678901");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldTest3() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Петров-Водкин Василий");
+        $("[data-test-id=phone] input").setValue("+09270000000");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldTest4() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Я-Ы");
+        $("[data-test-id=phone] input").setValue("+00000000000");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldTest5() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("-");
+        $("[data-test-id=phone] input").setValue("+22222222222");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldTest6() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Ы");
+        $("[data-test-id=phone] input").setValue("+11111111111");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldTest7() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Василий");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
